43edf369b54aa31a6cfa6e70bf659d4086646ba7
Glebkas <baran123456@gmail.com>
2022-03-07 19:42:15 +0200

    Update Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from './Hero';
+import Impact from './Impact';
+import Pricing from './Pricing';
+import Problem from './Problem';
+import Solution from './Solution';
+import Team from './Team';
+import WorkPlan from './WorkPlan';
+
+function Main() {
+    return (
+        <>
+            <Hero></Hero>
+            <Problem></Problem>
+            <Solution></Solution>
+            <WorkPlan></WorkPlan>
+            <Impact></Impact>
+            <Pricing></Pricing>
+            <Team></Team>
+        </>
+    );
+}
+
+export default Main;
