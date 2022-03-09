6fb86c48f752e2e60496bcdea76a59af2c72b424
Glebkas <baran123456@gmail.com>
2022-03-09 12:58:01 +0200

    Create Table.js

---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Table() {
+    return (
+        <div className='table'>
+            <h2 className='table__title'>The competition is no match</h2>
+            <div className='table__container'></div>
+        </div>
+    );
+}
+
+export default Table;
