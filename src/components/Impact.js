2ba11c46832c9ee9ee123486a50be0128b363971
Glebkas <baran123456@gmail.com>
2022-03-09 12:58:10 +0200

    Update Impact.js

--- src/components/Impact.js
+++ src/components/Impact.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from 'react';
+import data from '../utils/data';
+import PointsBox from './PointsBox';
 function Impact() {
-  return (
-    <div>Impact</div>
-  )
+    return (
+        <section id='impact' className='impact'>
+            <h2 className='impact__title'>{data.impact.title}</h2>
+            <div className='impact__points-container'>
+                {data.impact.pointsboxs.map((pointsbox, index) => (
+                    <PointsBox
+                        key={index}
+                        id={pointsbox.id}
+                        title={pointsbox.title}
+                        points={pointsbox.points}
+                    />
+                ))}
+            </div>
+            
+        </section>
+    );
 }
 
-export default Impact
+export default Impact;
