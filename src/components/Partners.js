58e9571cb571de40ea218a95dcc9c9962133df21
Glebkas <baran123456@gmail.com>
2022-03-09 12:58:07 +0200

    Create Partners.js

---
+++ src/components/Partners.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Partners() {
+    return (
+        <section>
+            <h2>Partners</h2>
+            <div></div>
+        </section>
+    );
+}
+
+export default Partners;
