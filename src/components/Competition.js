8bde9de2efbc536e4c6e1a780f014e33d46c478d
Glebkas <baran123456@gmail.com>
2022-03-10 21:58:34 +0200

    Create Competition.js

---
+++ src/components/Competition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Competition() {
+    return (
+        <section className='competition'>
+            <h2></h2>
+            Competiotion
+        </section>
+    );
+}
+
+export default Competition;
