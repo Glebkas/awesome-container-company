71c6a9f6def2619c28de0483512451b0ec7fa3cc
Glebkas <baran123456@gmail.com>
2022-03-10 20:51:15 +0200

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -30,6 +30,60 @@ const data = {
         subtitle:
             ' replaces single-use packaging with reusable stainless steel containers',
 
+        slider: [
+            {
+                image: '../images/container1.jpg',
+                featurepoints: [
+                    'Removable/adjustable divider',
+                    'Leak-resistant lid keeps food fresh',
+                    'Easy to sanitize and dishwasher safe',
+                    'Containers and lids are freezer safe',
+                    'Containers are oven safe up to 300 degrees',
+                    'Lighter and sturdier than glass',
+                    'Etched tare weights for easy bulk shopping',
+                    '304 (18/8) stainless steel, LDPE #4 plastic lid, PP #5 plastic divider',
+                    'Free of BPA, BPS, phthalates and lead',
+                ],
+                sizingpoints: [
+                    '25oz container size with lid on: 7.75” x 1.75" x 5” Lids: 7.75” x 5”',
+                ],
+            },
+            {
+                image: '../images/container2.jpg',
+                featurepoints: [
+                    'Leak-resistant lid keeps food fresh',
+                    'Easy to sanitize and dishwasher safe',
+                    'Containers and lids are freezer safe',
+                    'Containers are oven safe up to 300 degrees',
+                    'Three containers nest to save storage space',
+                    'Lighter and sturdier than glass',
+                    'Etched tare weights for easy bulk shopping',
+                    '304 (18/8) stainless steel, LDPE #4 plastic',
+                    'Free of BPA, BPS, phthalates and lead',
+                ],
+                sizingpoints: [
+                    '16oz, container size with lid on: 4-5/8" x 2.25". Lid is 4.75” diameter',
+                    '9oz, container size with lid on: 4" x 2". Lid is 4” diameter',
+                    '5oz, container size with lid on: 3.5" x 1.5". Lid is 3.5” diameter',
+                ],
+            },
+            {
+                image: '../images/container3.jpg',
+                featurepoints: [
+                    'Removable 2-section + 4-section divider',
+                    'Leak-resistant lid keeps food fresh',
+                    'Easy to sanitize and dishwasher safe',
+                    'Containers and lids are freezer safe',
+                    'Containers are oven safe up to 300 degrees',
+                    'Lighter and sturdier than glass',
+                    'Etched tare weights for easy bulk shopping',
+                    '304 (18/8) stainless steel, LDPE #4 plastic lid, PP#5 divider',
+                    'Free of BPA, BPS, phthalates and lead',
+                ],
+                sizingpoints: ['100oz (3 liters, 12.5 cups), 9.5” x 3.25”'],
+            },
+        ],
+
         ilustrations: {
             logistics: {
                 image: '../images/logistics.svg',
