8e54e2f8c3cee6cc6eec87e216df8d31d2a673e2
Glebkas <baran123456@gmail.com>
2022-03-10 21:58:40 +0200

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import data from '../utils/data';
 
 function Team() {
     return (
-        <section>
-            <h2>Team</h2>
+        <section className='team'>
+            <h2 className='team__title'>Team</h2>
+
+            <div className='team__container'>
+                {data.team.members.map((member) => (
+                    <div>
+                        <img></img>
+                        <p></p>
+                        <div></div>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 }
