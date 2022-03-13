81fe40888dd2e432c7b0dd16abc6d656b436dc64
Glebkas <baran123456@gmail.com>
2022-03-13 17:25:27 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,71 @@
 import React from 'react';
+import data from '../utils/data';
 
 function Footer() {
     return (
         <section id='contacts' className='footer'>
             <div className='footer__top'>
-                <div>
-                    <p></p>
+                <div className='footer-section'>
+                    <p className='footer-section__title'>
+                        {data.footer.section1.title}
+                    </p>
+                    <p className='footer-section__item'>
+                        {data.footer.section1.email}
+                    </p>
+                    <p className='footer-section__item'>
+                        {data.footer.section1.address}
+                    </p>
                 </div>
-                <div>
-                    <p></p>
+                <div className='footer-section'>
+                    <p className='footer-section__title'>
+                        {data.footer.section2.title}
+                    </p>
+                    {data.footer.section2.socials.map((social) => (
+                        <div className='footer-section__item'>
+                            <img className='footer-section__social-icon' src={social.image} alt='social link'></img>
+                            <a
+                                className='footer-section__item footer-section__item_type_link'
+                                href={social.url}
+                            >
+                                {social.title}
+                            </a>
+                        </div>
+                    ))}
                 </div>
-                <div>
-                    <p></p>
+                <div className='footer-section footer-section_type_place'>
+                    <p className='footer-section__title'>
+                        {data.footer.section3.title}
+                    </p>
+
+                    <p className='footer-section__item'>
+                        {data.footer.section3.currentplaces.title}
+                    </p>
+                    <div className='footer-section__place-container'>
+                        {data.footer.section3.currentplaces.places.map(
+                            (currentPlace) => (
+                                <p className='footer-section__item footer-section__item_type_place'>
+                                    {currentPlace}
+                                </p>
+                            )
+                        )}
+                    </div>
+                    <p className='footer-section__item'>
+                        {data.footer.section3.planedplaces.title}
+                    </p>
+                    <div className='footer-section__place-container'>
+                        {data.footer.section3.planedplaces.places.map(
+                            (planedPlace) => (
+                                <p className='footer-section__item footer-section__item_type_place'>{`${planedPlace},`}</p>
+                            )
+                        )}
+                    </div>
                 </div>
             </div>
-            <div className='footer__botom'>
-                <p></p>
-                <p></p>
+            <div className='footer__bottom'>
+                <p className='footer__bottom-item'>
+                    Awesome Container Compnay © {new Date().getFullYear()}
+                </p>
+                <p className='footer__bottom-item'>Terms of Use</p>
             </div>
         </section>
     );
