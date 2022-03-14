6196e862ab7b22762d00deef581a83809b2a053b
Glebkas <baran123456@gmail.com>
2022-03-14 10:42:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Contactform from './components/Contactform';
 
 function App() {
+    const [showPopup, setShowPopup] = React.useState(false);
+
+    const handlePopupToggle = () => {
+        setShowPopup(!showPopup);
+        console.log(showPopup);
+    };
+
     return (
         <>
-            <Header />
-            <Main/>
-            <Footer/>
-
+            {showPopup && <Contactform closePopup={handlePopupToggle} />}
+            <Header openPopUp={handlePopupToggle} />
+            <Main openPopUp={handlePopupToggle} />
+            <Footer />
         </>
     );
 }
