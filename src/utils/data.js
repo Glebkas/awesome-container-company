404be3df9e7a19595af0c2ccadb8a5265d0cedf3
Glebkas <baran123456@gmail.com>
2022-03-14 13:09:03 +0200

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -11,23 +11,23 @@ const data = {
         title: 'Single-use packaging: bad for the planet, bad for business',
         description:
             'Millions of tons of single-use packaging end up in landfills and oceans while only 9% of single-use waste gets recycled. Meanwhile, single-use containers are expensive and restaurants need to track inventory / restock weekly.',
-        images: {
-            image1: {
+        images: [
+            {
                 src: '../images/ocean-polution.png',
                 imagecaption:
                     'Single-use plastics pollute oceans and kill marine species',
             },
-            image2: {
+            {
                 src: '../images/landfills.png',
                 imagecaption:
                     'Single-use plastics pollute landfills and emit dangerous chemicals and greenshouse gases',
             },
-            image3: {
+            {
                 src: '../images/unrecyclable.png',
                 imagecaption:
                     'Not all of the single-use plastics are recyclable',
             },
-        },
+        ],
     },
     solution: {
         title: 'Sustainability Delivered',
