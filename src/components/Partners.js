16caae8b8510bf6280f088cebf97fb2973603ac6
Glebkas <baran123456@gmail.com>
2022-03-16 20:51:41 +0200

    Update Partners.js

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Partners(props) {
+function Partners({appData}) {
     return (
         <section className='partners'>
-            <h2 className='partners__title'>{props.appData.partners.title}</h2>
+            <h2 className='partners__title'>{appData.partners.title}</h2>
             <div className='partners__logos-container'>
-                {props.appData.partners.logos.map((logo, index) => (
+                {appData.partners.logos.map((logo, index) => (
                     <img
                         key={`partner${index}`}
                         className='partners__logo-image'
