ff9a7bccfc4518426b748e679816d98a128c2d05
Glebkas <baran123456@gmail.com>
2022-03-16 20:51:35 +0200

    Update Pricing.js

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PriceBox from './PriceBox';
 import data from '../utils/data';
 import ContactButton from './ContactButton';
 
@@ -8,31 +7,28 @@ function Pricing(props) {
         <section id='pricing' className='pricing'>
             <div className='pricing__top'>
                 <div className='pricing__title-container'>
-                    <h2 className='pricing__top-title'>{data.pricing.title}</h2>
+                    <h2 className='pricing__top-title'>
+                        {props.appData.pricing.title}
+                    </h2>
                     <p className='pricing__top-description'>
-                        {data.pricing.description}
+                        {props.appData.pricing.description}
                     </p>
                 </div>
                 <img
                     className='pricing__top-image'
-                    src={data.pricing.image}
+                    src={props.appData.pricing.image}
                     alt='container ilustration'
                 ></img>
             </div>
             <div className='pricing__bottom'>
-                <h3 className='pricing__bottom-title'>{data.pricing.title2}</h3>
-                {/* <div className='pricing__price-boxes-container'>
-                    {data.pricing.priceboxs.map((pricebox) => (
-                        <PriceBox
-                            key={pricebox.id}
-                            title={pricebox.title}
-                            price={pricebox.price}
-                        />
-                    ))}
-                </div> */}
+                <h3 className='pricing__bottom-title'>
+                    {props.appData.pricing.title2}
+                </h3>
             </div>
             <ContactButton openPopUp={props.openPopUp} class='contact-button' />
-            <p className='pricing__contact-subtitle'>{data.pricing.contact}</p>
+            <p className='pricing__contact-subtitle'>
+                {props.appData.pricing.contact}
+            </p>
         </section>
     );
 }
