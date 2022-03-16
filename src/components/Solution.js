a4781e2a083f91b629e653a95a4dc36b773a6eb6
Glebkas <baran123456@gmail.com>
2022-03-16 20:51:30 +0200

    Update Solution.js

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -1,55 +1,106 @@
 import React from 'react';
-import ImageCarousel from './ImageCarousel';
-import data from '../utils/data';
-function Solution(props) {
+import { MImageCarousel } from './ImageCarousel';
+import { motion } from 'framer-motion';
+import {
+    solutionCaruselAnimation,
+    solutionTextAnimation,
+    solutionIconAnimation,
+} from '../utils/animation';
+
+function Solution({ appData }) {
     return (
         <section id='solution' className='solution'>
-            <h2 className='solution__title'>{data.solution.title}</h2>
-            <p className='solution__subtitle'>
-                <span className='solution__subtitle solution__subtitle_type_orange'>
-                    {data.hero.companyname}
-                </span>
-                {data.solution.subtitle}
-            </p>
-            <ImageCarousel appData={props.appData}></ImageCarousel>
+            <motion.span
+                viewport={{ amount: 0.5, once: true }}
+                initial='hidden'
+                whileInView='visible'
+            >
+                <motion.h2
+                    variants={solutionTextAnimation}
+                    custom={1}
+                    className='solution__title'
+                >
+                    {appData.solution.title}
+                </motion.h2>
+                <motion.p
+                    variants={solutionTextAnimation}
+                    custom={2}
+                    className='solution__subtitle'
+                >
+                    <span className='solution__subtitle solution__subtitle_type_orange'>
+                        {appData.hero.companyname}
+                    </span>
+                    {appData.solution.subtitle}
+                </motion.p>
+            </motion.span>
+            <MImageCarousel
+                viewport={{ amount: 0.5, once: true }}
+                initial='hidden'
+                whileInView='visible'
+                variants={solutionCaruselAnimation}
+                appData={appData}
+            ></MImageCarousel>
 
             <div className='soulution__ilustrations'>
-                <div className='solution__icon-container'>
+                <motion.div
+                    viewport={{ amount: 0.5, once: true }}
+                    initial='hidden'
+                    whileInView='visible'
+                    variants={solutionIconAnimation}
+                    whileHover={{ scale: 1.1 }}
+                    className='solution__icon-container'
+                >
                     <div className='solution__icon-inner-container'>
                         <img
-                            src={data.solution.ilustrations.logistics.image}
+                            src={appData.solution.ilustrations.logistics.image}
                             className='solution__icon solution__icon_type_top'
-                            alt='data.solution.ilustrations.logistics.caption'
+                            alt={
+                                appData.solution.ilustrations.logistics.caption
+                            }
                         />
                         <p className='solution__icon-caption'>
-                            {data.solution.ilustrations.logistics.caption}
+                            {appData.solution.ilustrations.logistics.caption}
                         </p>
                     </div>
-                </div>
-                <div className='solution__icon-container solution__icon-container_type_center'>
+                </motion.div>
+                <motion.div
+                    viewport={{ amount: 0.5, once: true }}
+                    initial='hidden'
+                    whileInView='visible'
+                    variants={solutionIconAnimation}
+                    whileHover={{ scale: 1.1 }}
+                    className='solution__icon-container solution__icon-container_type_center'
+                >
                     <div className='solution__icon-inner-container'>
                         <img
-                            src={data.solution.ilustrations.costs.image}
+                            src={appData.solution.ilustrations.costs.image}
                             className='solution__icon solution__icon_type_midlle'
-                            alt={data.solution.ilustrations.costs.caption}
+                            alt={appData.solution.ilustrations.costs.caption}
                         />
                         <p className='solution__icon-caption'>
-                            {data.solution.ilustrations.costs.caption}
+                            {appData.solution.ilustrations.costs.caption}
                         </p>
                     </div>
-                </div>
-                <div className='solution__icon-container solution__icon-container_type_bottom'>
+                </motion.div>
+                <motion.div
+                    viewport={{ amount: 0.5, once: true }}
+                    initial='hidden'
+                    whileInView='visible'
+                    variants={solutionIconAnimation}
+                    whileHover={{ scale: 1.1 }}
+                    className='solution__icon-container solution__icon-container_type_bottom'
+                >
                     <div className='solution__icon-inner-container'>
                         <img
-                            src={data.solution.ilustrations.space.image}
+                            src={appData.solution.ilustrations.space.image}
                             className='solution__icon solution__icon_type_bottom'
-                            alt={data.solution.ilustrations.space.caption}
+                            alt={appData.solution.ilustrations.space.caption}
                         />
                         <p className='solution__icon-caption'>
-                            {data.solution.ilustrations.space.caption}
+                            {appData.solution.ilustrations.space.caption}
                         </p>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     );
