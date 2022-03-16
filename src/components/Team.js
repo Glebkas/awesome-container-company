4fa2e1de106f9865342bfff0cb9713cb80835203
Glebkas <baran123456@gmail.com>
2022-03-16 20:51:28 +0200

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Team(props) {
+function Team({appData}) {
     return (
         <section id='team' className='team'>
             <h2 className='team__title'>An Awesome Team</h2>
 
             <div className='team__container'>
-                {props.appData.team.members.map((member) => (
+                {appData.team.members.map((member) => (
                     <div key={member.id} className='team-member'>
                         <img
                             className='team-member__image'
