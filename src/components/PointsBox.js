b09251a8644ed80633ec777dc6f9275bcec08aa0
Glebkas <baran123456@gmail.com>
2022-03-16 20:51:38 +0200

    Update PointsBox.js

--- src/components/PointsBox.js
+++ src/components/PointsBox.js
@@ -1,20 +1,44 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { pointsBoxAnimation } from '../utils/animation';
 
 function PointsBox(props) {
     return (
-        <div className='points-box'>
-            <p className='points-box__title'>{props.title}</p>
+        <motion.div
+            viewport={{ amount: 0.5, once: true }}
+            initial='hidden'
+            whileInView='visible'
+            className='points-box'
+            variants={pointsBoxAnimation}
+            custom={1}
+        >
+            <motion.p
+                variants={pointsBoxAnimation}
+                className='points-box__title'
+                custom={1}
+            >
+                {props.title}
+            </motion.p>
             <ul className='points-box__list'>
                 {props.points.map((point, index) => (
                     <>
-                        <div className='points-box__point' />
-                        <li key={index} className='points-box__list-item'>
+                        <motion.div
+                            variants={pointsBoxAnimation}
+                            custom={index + 1}
+                            className='points-box__point'
+                        />
+                        <motion.li
+                            variants={pointsBoxAnimation}
+                            custom={index + 1}
+                            key={index}
+                            className='points-box__list-item'
+                        >
                             {point}
-                        </li>
+                        </motion.li>
                     </>
                 ))}
             </ul>
-        </div>
+        </motion.div>
     );
 }
 
