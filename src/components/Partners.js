f2382740ddee0a49a8b01da241b22cca8e54ee14
Glebkas <baran123456@gmail.com>
2022-03-22 21:48:03 +0200

    Update Partners.js

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { textAnimation, partnerAnimation } from '../utils/animation';
 
-function Partners({appData}) {
+function Partners({ appData }) {
     return (
-        <section className='partners'>
-            <h2 className='partners__title'>{appData.partners.title}</h2>
+        <motion.section
+            viewport={{ amount: 0.23, once: true }}
+            initial='hidden'
+            whileInView='visible'
+            className='partners'
+        >
+            <motion.h2 variants={textAnimation} className='partners__title'>
+                {appData.partners.title}
+            </motion.h2>
             <div className='partners__logos-container'>
                 {appData.partners.logos.map((logo, index) => (
-                    <img
+                    <motion.img
+                        variants={partnerAnimation}
+                        custom={index + 3}
                         key={`partner${index}`}
                         className='partners__logo-image'
                         alt='partner '
                         src={logo}
-                    ></img>
+                    ></motion.img>
                 ))}
             </div>
-        </section>
+        </motion.section>
     );
 }
 
