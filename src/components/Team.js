f332e224c451c2d02a527b48de7366a41e1076f8
Glebkas <baran123456@gmail.com>
2022-03-22 21:48:11 +0200

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { teamAnimation, textAnimation } from '../utils/animation';
 
-function Team({appData}) {
+function Team({ appData }) {
     return (
-        <section id='team' className='team'>
-            <h2 className='team__title'>An Awesome Team</h2>
+        <motion.section
+            viewport={{ amount: 0.23, once: true }}
+            initial='hidden'
+            whileInView='visible'
+            id='team'
+            className='team'
+        >
+            <motion.h2 variants={textAnimation} className='team__title'>
+                An Awesome Team
+            </motion.h2>
 
             <div className='team__container'>
-                {appData.team.members.map((member) => (
-                    <div key={member.id} className='team-member'>
+                {appData.team.members.map((member, index) => (
+                    <motion.div
+                        variants={teamAnimation}
+                        custom={index + 3}
+                        key={member.id}
+                        className='team-member'
+                    >
                         <img
                             className='team-member__image'
                             alt={member.name}
@@ -16,10 +31,10 @@ function Team({appData}) {
                         <p className='team-member__name'>{member.name}</p>
                         <p className='team-member__title'>{member.title}</p>
                         <div className='team-member__link'></div>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
-        </section>
+        </motion.section>
     );
 }
 
