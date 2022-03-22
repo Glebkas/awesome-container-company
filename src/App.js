b03451ccb787ca5668c4078b6c9819b0d9509645
Glebkas <baran123456@gmail.com>
2022-03-22 08:17:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import ContactForm from './components/ContactForm';
+import { MContactForm } from './components/ContactForm';
 import data from './utils/data';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
     const [showPopup, setShowPopup] = React.useState(false);
@@ -16,7 +17,18 @@ function App() {
 
     return (
         <>
-            {showPopup && <ContactForm closePopup={handlePopupToggle} />}
+            <AnimatePresence>
+                {showPopup && (
+                    <MContactForm
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: 'auto', opacity: 1 }}
+                        transition={{ duration: 0.6 }}
+                        exit={{ opacity: 0 }}
+                        closePopup={handlePopupToggle}
+                    />
+                )}
+            </AnimatePresence>
+
             <Header openPopUp={handlePopupToggle} />
             <Main appData={appData} openPopUp={handlePopupToggle} />
             <Footer appData={appData} />
