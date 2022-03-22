51a63bb5cc0769aad50529c73d2360d758ef70c5
Glebkas <baran123456@gmail.com>
2022-03-22 21:48:00 +0200

    Update Impact.js

--- src/components/Impact.js
+++ src/components/Impact.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import PointsBox from './PointsBox';
+import { motion } from 'framer-motion';
+import { textAnimation } from '../utils/animation';
+
 function Impact(props) {
     return (
         <section id='impact' className='impact'>
-            <h2 className='impact__title'>{props.appData.impact.title}</h2>
+            <motion.h2
+                viewport={{ amount: 0.23, once: true }}
+                initial='hidden'
+                whileInView='visible'
+                variants={textAnimation}
+                className='impact__title'
+            >
+                {props.appData.impact.title}
+            </motion.h2>
             <div className='impact__points-container'>
                 {props.appData.impact.pointsBoxs.map((pointsBox, index) => (
                     <PointsBox
                         key={pointsBox.id}
-                        id={pointsBox.id}
+                        id={pointsBox.id + index}
                         title={pointsBox.title}
                         points={pointsBox.points}
+                        backgroundImage ={pointsBox.backgroundImage}
                     />
                 ))}
             </div>
