04d4491ae7f4a608a8fe6d8f6c6ba62d70c6e481
Glebkas <baran123456@gmail.com>
2022-03-24 08:15:39 +0200

    Update WorkPlan.js

--- src/components/WorkPlan.js
+++ src/components/WorkPlan.js
@@ -1,13 +1,53 @@
-import React from 'react'
-import data from '../utils/data'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { textAnimation, workPlanAnimation } from '../utils/animation';
 
-
-function WorkPlan() {
-  return (
-    <div className='workplan'>
-      <h2 className='workplan_title'>{data.hero.title}</h2>
-      </div>
-  )
+function WorkPlan(props) {
+    return (
+        <motion.div
+            viewport={{ amount: 0.2, once: true }}
+            initial='hidden'
+            whileInView='visible'
+            className='workplan'
+        >
+            <motion.h2
+                variants={textAnimation}
+                custom={1}
+                className='workplan__title'
+            >
+                {props.appData.workPlan.title}
+            </motion.h2>
+            <motion.div className='workplan__ilustrations-container'>
+                {props.appData.workPlan.steps.map((step, index) => (
+                    <React.Fragment key={`workplanStep${index}`}>
+                        <motion.div
+                            variants={workPlanAnimation}
+                            custom={index + 1}
+                            className={`workplan__ilustration-container workplan__ilustration-container_type_${step.classType}`}
+                        >
+                            <img
+                                src={step.image}
+                                className='workplan__ilustration-image'
+                                alt={step.caption}
+                            ></img>
+                            <p className='workplan__ilustration-number'>
+                                {step.number}
+                            </p>
+                            <p className='workplan__ilustration-caption'>
+                                {step.caption}
+                            </p>
+                        </motion.div>
+                        <motion.div
+                            variants={workPlanAnimation}
+                            custom={index + 1}
+                            className={`workplan__arrow workplan__arrow_type_${step.classType}`}
+                        />
+                    </React.Fragment>
+                ))}
+                <p className='workplan__caption'>Circular ecosystem</p>
+            </motion.div>
+        </motion.div>
+    );
 }
 
-export default WorkPlan
+export default WorkPlan;
